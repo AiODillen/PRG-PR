f66581775aa8ce190770b1eb42213129a8b2381a
niklas <niklasdillenberger@gmail.com>
2018-10-26 21:42:31 +0200

    rebuild with constructors

--- PRG-PR/Aufgabe01c.cpp
+++ PRG-PR/Aufgabe01c.cpp
@@ -4,20 +4,58 @@ using namespace std;
 
 class GameofLife
 {
-  int width;
-  int length;
-  int feld[30][30];
+  int width, length;
 public:
-  void set_size(int a, int b);
+  GameofLife();
+  GameofLife(int, int);
+  void print(int**);
+  int **generate_2d_array();
 };
 
-void GameofLife::set_size(int a , int b)
-{
-  width = a;
-  length = b;
+GameofLife::GameofLife(int x, int y) {width = x; length = y;}
+GameofLife::GameofLife() {width = 30; length = 30;}
+
+int** GameofLife::generate_2d_array() {
+    cout << "Matrix " << width << "x" << length << " was created:" << endl;
+    int ** array2D;
+    array2D = new int*[width];
+    for (int i = 0; i < width; i++) {
+        array2D[i]= new int[length];
+        for (int j = 0; j < length; j++) {
+            array2D[i][j] = 0;
+        }
+    }
+    return array2D;
 }
+
+void GameofLife::print(int** array)
+{
+  cout << endl;
+  for (int x=0; x<width; x++){
+      for (int y=0; y<length; y++){
+          cout << array[x][y] << " ";
+      }
+      cout << endl;
+  }
+  cout << endl;
+}
+
 int main()
 {
-  GameofLife feld;
+  string i; cout << "Eigene Größe festlegen? >>  "; cin >> i;
+  if (i=="N" || i=="n"){
+    GameofLife feld;
+    int** new_array = feld.generate_2d_array();
+    new_array[4][4] = 1;
+    feld.print(new_array);
+  }
+  else if (i=="Y" || i=="y"){
+    int j, h; cout << endl; cout << "Länge? >>  "; cin >> j; cout << endl;
+    cout << "Breite? >>  "; cin >> h; cout << endl;
+    GameofLife feld (j, h);
+    int** new_array = feld.generate_2d_array();
+    new_array[4][4] = 1;
+    feld.print(new_array);
+  }
   return 0;
 }
